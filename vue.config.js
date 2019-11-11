696f6eb56ae79be80af1d2eeccbf2016d59e1923
BK1917songqian <2304637663@qq.com>
2019-11-11 11:53:51 +0800

    项目初始化

---
+++ vue.config.js
@@ -0,0 +1,30 @@
+const path =require("path")
+module.exports={
+    devServer:{
+        "/Service":{
+            target:"http://m.mtime.cn/Service/callback.mi/Showtime/LocationMovies.api",
+            changeOrigin:true
+        }
+    },
+    configureWebpack:{
+        alias:{
+            "@":path.join(__dirname,"./src"),
+            //接口
+            "@api":path.join(__dirname,"./src/api"),
+            //静态文件
+            "@assets":path.join(__dirname,"./src/assets"),
+            //公共的文件样式
+            "@common":path.join(__dirname,"./src/common"),
+            //创建组件
+            "@components":path.join(__dirname,"./src/components"),
+            //每一个页面
+            "@pages":path.join(__dirname,"./src/pages"),
+            //路由
+            "@router":path.join(__dirname,"./src/router"),
+            //vuex
+            "@store":path.join(__dirname,"./src/store"),
+            //发布方法的封装
+            "@utils":path.join(__dirname,"./src/utils"),
+        }
+    }
+}
